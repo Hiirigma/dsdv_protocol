a994db7bac6417636c6ca9a674b6a0c49991f223
Hiirigma <43312107+Hiirigma@users.noreply.github.com>
2022-05-11 23:37:43 +0300

    Fix node's attributes set up

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# TODO:
-# + random graph generation
-# + node choosing
-# + generate route
-# Destination-Sequenced Distance Vector Routing 
 import networkx as nx
 import matplotlib.pyplot as plt
 import random
@@ -35,16 +30,20 @@ def GenerateRandomGraph():
  
     color_map = [g_Type[random.randint(0,2**32 - 1) % len(g_Type)] for node in gG]
 
+    # convert list to dict to attach with every node as attribute
+    res_dct = {i: color_map[i] for i in range(0, len(color_map))}
+    nx.set_node_attributes(gG, res_dct, name="color")
+
     dRouteCnt = random.randint(4,10)
 
     print (f"Route count: {dRouteCnt}")
 
-
+    # set 
     for n in range(dNodeCnt):
         for nn in gG.neighbors(n):
             edge_attrs = {}
             edge_attrs[(n, nn)] = 'black'
-            nx.set_edge_attributes(gG, edge_attrs, "edge_color")
+            nx.set_edge_attributes(gG, edge_attrs, "color")
 
     for cnt in range(4):
         d_groups = []
@@ -62,7 +61,7 @@ def GenerateRandomGraph():
                 if saved_color != color_map[n] and n not in d_nodes:
                     edge_attrs = {}
                     edge_attrs[(cur_node, n)] = g_Type[cnt]
-                    nx.set_edge_attributes(gG, edge_attrs, "edge_color")
+                    nx.set_edge_attributes(gG, edge_attrs, "color")
                     cur_node = n
                     print (f"{cnt}: New node: {cur_node}")
                     break
@@ -72,7 +71,14 @@ def GenerateRandomGraph():
 
             pass
 
-    edge_colors = nx.get_edge_attributes(gG,'edge_color').values()
+    edge_colors = nx.get_edge_attributes(gG,'color').values()
+
+    print("edges")
+    print(list(gG.edges(data=True)))
+    print("default graph attributes")
+    print(gG.graph)
+    print("node attributes")
+    print(gG.nodes.data(True))
     nx.draw_networkx(gG, with_labels=True, node_color=color_map, edge_color=edge_colors, width=3, node_size=300)
     
     plt.show()
